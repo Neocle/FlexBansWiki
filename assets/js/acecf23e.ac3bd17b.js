86d5e36abf3e570ad1e2dc5db56c8005d6e56556
Neocle <lilian433700@gmail.com>
2026-02-14 22:02:24 +0100

    Deploy website - based on cdc73b19e091343797a628ae06fa3685d0cd4f76

---
+++ assets/js/acecf23e.ac3bd17b.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkflex_bans_wiki=globalThis.webpackChunkflex_bans_wiki||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/FlexBansWiki/blog","blogTitle":"Blog","authorsListPath":"/FlexBansWiki/blog/authors"}')}}]);
---
+++ assets/js/e95e6efe.2673076f.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkflex_bans_wiki=globalThis.webpackChunkflex_bans_wiki||[]).push([[8019],{5785(a){a.exports=JSON.parse('{"metadata":{"permalink":"/FlexBansWiki/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
